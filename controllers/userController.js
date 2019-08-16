a79e7e2c25ccbf0cbf9b7953e67cd86154613477
Yang <ybh1760@gmail.com>
2019-08-16 18:47:40 +0900

    Template Variables in pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
-export const join = (rep,res)=>res.render("join");
-export const login = (rep,res)=>res.render("login");
-export const logout = (rep,res)=>res.render("logout");
+export const join = (rep,res)=>res.render("join", {pageTitle : "Join"});
+export const login = (rep,res)=>res.render("login", {pageTitle : "Log In"});
+export const logout = (rep,res)=>res.render("logout", {pageTitle : "Log Out"});
 
-export const users = (rep, res)=>res.render("users");
-export const userDetail = (rep, res)=>res.render("userDetail");
-export const editProfile = (rep, res)=>res.render("editProfile");
-export const changePassword = (rep, res)=>res.render("changePassword");
+export const users = (rep, res)=>res.render("users", {pageTitle : "Users"});
+export const userDetail = (rep, res)=>res.render("userDetail", {pageTitle : "User Detail"});
+export const editProfile = (rep, res)=>res.render("editProfile", {pageTitle : "Edit Profile"});
+export const changePassword = (rep, res)=>res.render("changePassword", {pageTitle : "Change Password"});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,8 @@
-export const home =(rep,res)=> res.render("home");
-export const search =(rep,res)=> res.render("search");
+export const home =(rep,res)=> res.render("home", {pageTitle : "Home"});
+export const search =(rep,res)=> res.render("search", {pageTitle : "Search"});
 
-export const videos=(rep,res)=>res.render("videos");
-export const upload=(rep,res)=>res.render("upload");
-export const videoDetails=(rep,res)=>res.render("videoDetails");
-export const editVideo=(rep,res)=>res.render("editVideo");
-export const deleteVideo=(rep,res)=>res.render("deleteVideo");
+export const videos=(rep,res)=>res.render("videos", {pageTitle : "Videos"});
+export const upload=(rep,res)=>res.render("upload", {pageTitle : "Upload"});
+export const videoDetails=(rep,res)=>res.render("videoDetails", {pageTitle : "Video Details"});
+export const editVideo=(rep,res)=>res.render("editVideo", {pageTitle : "Edit Video"});
+export const deleteVideo=(rep,res)=>res.render("deleteVideo", {pageTitle : "Delete Video"});
