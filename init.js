3c2c7b516209652e829e14a72c175a24becb12a1
Yang <ybh1760@gmail.com>
2019-08-26 17:46:15 +0900

    Install  MongoDB & Dotenv

--- init.js
+++ init.js
@@ -1,5 +1,8 @@
 import app from "./app";
+import "./db";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=>console.log(`Listening on : http://localhost:${PORT}`));
