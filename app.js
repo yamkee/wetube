a8efd0211d3de95a7b613ed38d8b590d5eb63b6e
Yang <ybh1760@gmail.com>
2019-09-07 14:23:05 +0900

    Loggin the User In

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import passport from "passport";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import { localMiddleware } from "./middlewares";
+import "./passport";
 
 const app = express();
 
@@ -19,6 +21,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
